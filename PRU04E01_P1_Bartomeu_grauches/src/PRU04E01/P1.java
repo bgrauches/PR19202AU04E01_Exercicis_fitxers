12233aaac3e4de6463df2216d4fd0a8539309ed2
bgrauches <57674460+bgrauches@users.noreply.github.com>
2020-02-12 20:52:35 +0100

    te falta ordernar alfabeticament

--- PRU04E01_P1_Bartomeu_grauches/src/PRU04E01/P1.java
+++ PRU04E01_P1_Bartomeu_grauches/src/PRU04E01/P1.java
@@ -3,15 +3,18 @@ package PRU04E01;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-
 
 public class P1 {
 	
 	static ArrayList<String> jocs = new ArrayList<String>(); 
-	static ArrayList<Byte> abecedari = new ArrayList<Byte>();
+	int palabra;
+	int palabraComp;
+	int aux;
 	
 	
 	//Metode per llegir arxiu 
@@ -29,24 +32,54 @@ public class P1 {
 			llegir.close();
 			
 		} 
-		catch (FileNotFoundException e) {
+		catch (FileNotFoundException a) {
 			System.out.println("No es troba el fitxer Videojoggos.txt");
 		}
-		catch (IOException f) { 
-			System.out.println("No es pot llegir fruita.txt");
+		catch (IOException b) { 
+			System.out.println("No es pot llegir Videojoggos.txt");
 		}
 	
 	}
 	
 	//Metode per ordenar alfabèticament
-	public void ordenarAlf() {
-		<String>.tocharArray();
+	public void ordenarAlf(int [] A) {
+		for (palabra = 0; palabra < jocs.size()-palabra-1; palabraComp++) {
+			for (palabraComp = 0; palabraComp<jocs.size()-palabra-1; palabraComp ++) {
+				if (jocs [palabraComp +1] <jocs [palabraComp]) {
+					aux = A [palabraComp+1];
+					jocs [palabraComp +1] = jocs [palabraComp];
+					jocs [palabraComp] = aux;
+				}
+			}
+		}
 		
 	}
 	
+	//Metode per escriure les paraules ordenades a un altre arxiu
+	public void escriureFie() throws IOException{
+		
+		try {
+			FileOutputStream outpStr = new FileOutputStream("C:\\Users\\bgrauches\\Desktop\\LlistaOrdenada.txt");
+			OutputStreamWriter outWrite = new OutputStreamWriter(outpStr); 
+			
+			for(int i = 0 ; i < jocs.size(); i++ ) {
+				outWrite.write(jocs.get(i)+"\n");
+			}
+			outWrite.close();
+		}
+		catch (FileNotFoundException a) {
+			System.out.println("No es troba el fitxer LlistaOrdenada.txt");
+		}
+		
+		catch(IOException e) { 
+			System.out.println("No puc escriure en LlistaOrdenada.txt");
+		}
+	}
+			
+			
+	
 
 	public static void main(String[] args) throws IOException {
-			
 			llegirFile();
 	}
 }

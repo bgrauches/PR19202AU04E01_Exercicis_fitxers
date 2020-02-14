85866ccf8e95be12f27340839043d2f8f1609994
bgrauches <57674460+bgrauches@users.noreply.github.com>
2020-02-14 18:21:40 +0100

    no te pirula

--- PRU04E01_P1_Bartomeu_grauches/src/PRU04E01/P1.java
+++ PRU04E01_P1_Bartomeu_grauches/src/PRU04E01/P1.java
@@ -15,32 +15,43 @@ public class P1 {
 	static ArrayList<String> jocs = new ArrayList<String>(); 
 	static ArrayList<String> llistaOrdenada = new ArrayList<String>(); 
 	
+	//contadors de l'array
+	static int contadorJocs1;
+	static int contadorJocs2;
+	
 	//Pillam paraules de les posicions 0 i 1 de l'array d'entrada
-	static String paraula0 = jocs.get(0);
-	static String paraula1 = jocs.get(1);
+	static String paraula0;
+	static String paraula1;
 	
 	//Feim array de les lletres que formen la paraula
-	static char[] charParaula0 = paraula0.toCharArray();
-	static char[] charParaula1 = paraula1.toCharArray();
+	static char[] charParaula0; //= paraula0.toCharArray(); //tira excepcion
+	static char[] charParaula1; //= paraula1.toCharArray();
 	
 	//Transformam a ASCII els caràcters de les posicions 0 de cada paraula, es plan es compararles
-	static int ascii1 = charParaula0[0];
-	static int ascii2 = charParaula1[0]; 
+	static int ascii1; //= charParaula0[0];
+	static int ascii2; //= charParaula1[0]; 
 	
 	//String on se guarda la paraula0 en cas de que sigui > a paraula1
 	static String paraulaAux;
-	
-	static char newChar1 = (char) ascii1;
-	static char newChar2 = (char) ascii2;
+
 
 	//Per cambiar les posicions depenent de si ascii es menor o major
 	public static void metodeGuay() {
-		if (ascii1 > ascii2) {
-			ascii1 = char1.se
+		if (ascii2 < ascii1) {
+			//guardam paraula0 a auxiliar pq no deletei
+			paraulaAux = paraula0;
+			
+			//posam paraula1 a la posició paraula0
+			paraula0 = paraula1;
+			
+			//posar paraula0(paraulaAux) a la posició paraula1
+			paraula1 = paraulaAux;
 		}
+	
 		else {
-		
+			System.out.println("Se queden on están");
 		}
+		
 	}
 	
 	
@@ -51,13 +62,13 @@ public class P1 {
 			FileInputStream inputStr = new FileInputStream("C:\\Users\\bgrauches\\Desktop\\Videojoggos.txt");
 			InputStreamReader inputRead = new InputStreamReader(inputStr);
 			BufferedReader llegir = new BufferedReader(inputRead);
-			String linia = "";
+			String linia = llegir.readLine();
 			while (linia != null) {
+				jocs.add(llegir.readLine());
 				System.out.println(linia);
 				linia = llegir.readLine();
 			}
 			llegir.close();
-			
 		} 
 		catch (FileNotFoundException a) {
 			System.out.println("No es troba el fitxer Videojoggos.txt");
@@ -65,19 +76,20 @@ public class P1 {
 		catch (IOException b) { 
 			System.out.println("No es pot llegir Videojoggos.txt");
 		}
-	
 	}
 	
 	//Metode per ordenar alfabèticament
-	public void ordenarAlf(int [] jocs) {
-		int tam = jocs.length;
+	public static void ordenarAlf() {
 		
 		//comprova array jocs
-		for (paraula0. = 0; paraula0 < tam - 1; ascii1++) {
+		for (contadorJocs1 = 0; contadorJocs1 < jocs.size() - 1; contadorJocs1++) {
 			
 			//comprova chars
-			for (ascii2 = 0; ascii2 < tam - ascii2 - 1; ascii2++) {
-				
+			for (contadorJocs2 = 0; contadorJocs2 < jocs.size() - contadorJocs2 - 1; contadorJocs2++) {
+				paraula0 = jocs.get(contadorJocs1);
+				  paraula1 = jocs.get(contadorJocs2);
+				//metodeguay
+				metodeGuay();
 			}
 		}
 		
@@ -86,14 +98,14 @@ public class P1 {
 	}
 	
 	//Metode per escriure les paraules ordenades a un altre arxiu
-	public void escriureFie() throws IOException{
+	public static void escriureFie() throws IOException{
 		
 		try {
 			FileOutputStream outpStr = new FileOutputStream("C:\\Users\\bgrauches\\Desktop\\LlistaOrdenada.txt");
 			OutputStreamWriter outWrite = new OutputStreamWriter(outpStr); 
 			
 			for(int i = 0 ; i < jocs.size(); i++ ) {
-				outWrite.write(jocs.get(i)+"\n");
+				outWrite.write(llistaOrdenada.get(i)+"\n");
 			}
 			outWrite.close();
 		}
@@ -106,10 +118,12 @@ public class P1 {
 		}
 	}
 			
-			
-	
 
 	public static void main(String[] args) throws IOException {
 			llegirFile();
+			System.out.println(jocs);
+			ordenarAlf();
+			escriureFie();
+			System.out.println(llistaOrdenada);
 	}
 }
